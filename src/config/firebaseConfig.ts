fa746483e14d2d7de2d15fc4a3e55135beecfa7e
Ivan Henriquez <ihenriquez@licitalab.cl>
2025-12-14 02:17:01 -0300

    feat: migrate Expo configuration to `app.config.js` and integrate Firebase environment variables via `expo-constants`.

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import Constants from "expo-constants";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: Constants.expoConfig?.extra?.apiKey || "",
+  authDomain: Constants.expoConfig?.extra?.authDomain || "",
+  projectId: Constants.expoConfig?.extra?.projectId || "",
+  storageBucket: Constants.expoConfig?.extra?.storageBucket || "",
+  messagingSenderId: Constants.expoConfig?.extra?.messagingSenderId || "",
+  appId: Constants.expoConfig?.extra?.appId || "",
+  measurementId: Constants.expoConfig?.extra?.measurementId || "",
 };
 
 // Initialize Firebase
